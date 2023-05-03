7eee0581e45f1007b625185273d16338b2406baf
creeper-0910 <56744841+creeper-0910@users.noreply.github.com>
2023-05-03 17:22:48 +0900

    first commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+/**
+ * Welcome to Cloudflare Workers! This is your first worker.
+ *
+ * - Run `npx wrangler dev src/index.js` in your terminal to start a development server
+ * - Open a browser tab at http://localhost:8787/ to see your worker in action
+ * - Run `npx wrangler publish src/index.js --name my-worker` to publish your worker
+ *
+ * Learn more at https://developers.cloudflare.com/workers/
+ */
+
+export default {
+	async fetch(request, env, ctx) {
+		const { searchParams } = new URL(request.url)
+		let name = searchParams.get('app_version')
+		console.log(name)
+		return new Response(name);
+	},
+};

81f1919ab82e89b262364a595ff2e2d55e359497
creeper-0910 <56744841+creeper-0910@users.noreply.github.com>
2023-05-05 19:46:48 +0900

    rewrite update server thanks comamoca!

---
+++ src/index.ts
@@ -0,0 +1,104 @@
+import { Hono } from "hono";
+
+// アップデート情報を配信するサンプルコード
+// 最新バージョンは0.1.0と仮定
+// URLクエリにバージョンを指定してアクセスすると、バージョンに応じて異なるJsonを返す
+
+// もしバージョンが最新でなかった場合はGitHub上に配置されているJSONのURLを返す
+// 最新の場合は`latest`という文字列を返す
+
+// 返却するJSONの定義
+interface API {
+  version: string | null;
+  updateURL: string | null;
+  updatePageURL: string | null;
+  releaseNotes: string | null;
+  moreInfoURL: string | null;
+  forced: boolean;
+  dotnetVersion: null;
+  dotnetDownloadLink: null;
+  dotnetOfficialLink: null;
+  versionTitle: string | null;
+  major: boolean;
+}
+
+const app = new Hono();
+
+// バージョンの表記方法を定義
+class Version {
+  major: number;
+  minor: number;
+  patch: number;
+
+  constructor(version: string) {
+    const [major, minor, patch] = version.split(".");
+
+    this.major = Number(major);
+    this.minor = Number(minor);
+    this.patch = Number(patch);
+  }
+
+  public toString(): string {
+    return `${this.major}.${this.minor}.${this.patch}`;
+  }
+
+  isOlder(version: Version): boolean {
+    // バージョン比較関数
+    // 与えられたバージョより古い場合はtrueを返す
+    console.log(Number.isInteger(version.patch));
+    console.log(Number.isInteger(this.patch));
+    if (this.major !== version.major) {
+      return true;
+    } else if (this.minor !== version.minor) {
+      return true;
+    } else if (this.patch !== version.patch) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
+
+app.get("/", (c) => {
+  const version = new Version(String(c.req.query("app_version")));
+  const latest = new Version("4.0.819");
+  if (version.isOlder(latest)) {
+    const update_url = new URL(
+      "https://github.com/creeper-0910/adg_update_srv/releases/download/4.0.819/adguard_4.0.819.apk",
+    );
+
+    const content: API = {
+      version: latest.toString(),
+      updateURL: update_url.toString(),
+      updatePageURL: "https://kb.adguard.com/general/adguard-beta-testing-program",
+      releaseNotes: "これは、AdGuardのNightlyビルドの更新です。このアップデートチャネルは不安定であることをご了承ください。\r\n\r\n## バージョン 4.0 Nightly 97\r\n\r\n* [Fixed] JP Hack!",
+      moreInfoURL: "* [Fixed] JP Hack!",
+      forced: false,
+      dotnetVersion: null,
+      dotnetDownloadLink: null,
+      dotnetOfficialLink: null,
+      versionTitle: "4.0 Nightly 99",
+      major: false,
+    };
+
+    return c.json(content);
+  } else {
+    const content: API = {
+      version: null,
+      updateURL: null,
+      updatePageURL: null,
+      releaseNotes: null,
+      moreInfoURL: null,
+      forced: false,
+      dotnetVersion: null,
+      dotnetDownloadLink: null,
+      dotnetOfficialLink: null,
+      versionTitle: null,
+      major: false,
+    };
+
+    return c.json(content);
+  }
+});
+
+export default app

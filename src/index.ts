3cca1b5754288e66ab1991ff7968dae5caf2e22e
creeper-0910 <creeper-0910@users.noreply.github.com>
2023-08-26 00:40:54 +0000

    Update Update-Server

--- src/index.ts
+++ src/index.ts
@@ -23,8 +23,8 @@ interface API {
 }
 
 const app = new Hono();
-const latest_version = "4.2.51"
-const latest_display_version = "4.2 Nightly 25"
+const latest_version = "4.2.56"
+const latest_display_version = "4.2 Nightly 26"
 
 // バージョンの表記方法を定義
 class Version {
